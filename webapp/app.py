ea420d970bcad728dbcacce639de88087084437d
kriegersaurusrex <kriegersaurusrex@gmail.com>
2021-02-02 18:56:54 -0500

    add-feat-list

--- webapp/app.py
+++ webapp/app.py
@@ -6,21 +6,36 @@ from flask import Flask, render_template
 
 from .models.hosts import Host, Listing
 
+import requests
+
 
 def create_app():
 
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite3'
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False    
+    
+    features = ["id", "room_type", "accommodates", "bathrooms", "bed_type", "cancellation_policy", "cleaning_fee", "city", "instant_bookable", "number_of_reviews",
+                    "review_scores_rating", "bedrooms", "beds"]
 
     @app.route('/')
     def root():
         """WebApp home page"""
+        #Show the actual lanfding page with fields to enter
         return render_template('index.html', title="Home")
 
-    @app.route('/add_listing')
+
+    @app.route('/add_listing', methods=["POST", "GET"])
     def add_listing():
         """Add a new listing to the database"""
+        #if(request.method == "POST"):
+            """Save the user input to the user DB"""
+
+        else:
+            
+
         return render_template('listing.html', title="Add a Listing", message="Coming Soon!")
 
     @app.route('/predict', methods=["POST"])

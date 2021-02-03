e2b9d8d4e67a1583828926dcf5d511ea97f822da
Kevin Weatherwalks <kevin.weatherwalks@gmail.com>
2021-02-02 19:29:28 -0500

    add dummy features to listing

--- webapp/app.py
+++ webapp/app.py
@@ -2,26 +2,38 @@
 
 from os import getenv
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from .models.hosts import Host, Listing
 
 
 def create_app():
 
+    # Instantiate flask app object
     app = Flask(__name__)
 
+    # Set DB environment variables
     app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+
+    # Save data (for development purposes)
+    listing = {}
 
     @app.route('/')
     def root():
         """WebApp home page"""
         return render_template('index.html', title="Home")
 
-    @app.route('/add_listing')
+    @app.route('/add_listing', methods=["GET", "POST"])
     def add_listing():
         """Add a new listing to the database"""
-        return render_template('listing.html', title="Add a Listing", message="Coming Soon!")
+        if request.method == "POST":
+            data = request.form.to_dict(flat=False)
+            print(data)
+            for key, value in data.items():
+                print(key, value)
+                listing[key] = value
+            print(listing)
+        return render_template('listing.html', title="Add a Listing", message=f"{listing}")
 
     @app.route('/predict', methods=["POST"])
     def predict():

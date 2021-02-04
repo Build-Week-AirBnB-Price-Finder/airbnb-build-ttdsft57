86296883de57ce9015dc9306d935e1f1ca07ef17
Kevin Weatherwalks <kevin.weatherwalks@gmail.com>
2021-02-03 20:57:42 -0500

    add prediction functionality to predict-one route

--- webapp/app.py
+++ webapp/app.py
@@ -1,17 +1,19 @@
 """Main app/routing file for AIrBnB Price Finder."""
 
+import os
+from os import getenv
+from tempfile import mkdtemp
+
+import numpy as np
+import pandas as pd
 from category_encoders import OrdinalEncoder
+from flask import Flask, jsonify, render_template, request
+from joblib import load
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import make_pipeline
-from sklearn.ensemble import RandomForestRegressor
-import pandas as pd
-from os import getenv
-import numpy as np
-import os
-from joblib import load
-from flask import Flask, render_template, request
+
 from .models import DB, Host, Listing
-from tempfile import mkdtemp
 
 
 def create_app():
@@ -34,13 +36,17 @@ def create_app():
     #   - options:          if type is choice
     #   - min, max, step:   if type is number
     features = {
+        "property_type": {"label": "Property Type",
+                          "type": "choice",
+                          "options": ['Apartment', 'House', 'Condominium',
+                                      'Townhouse', 'Loft']
+                          },
         "city": {"label": "City",
                  "type": "choice",
                  "options": ['NYC', 'LA', 'SF', 'DC', 'Chicago']},
         "room_type": {"label": "Room Type",
                       "type": "choice",
-                      "options": ['Apartment', 'House', 'Condominium',
-                                  'Townhouse', 'Loft']},
+                      "options": ['Entire home/apt', 'Private room', 'Shared room']},
         "accommodates": {"label": "Accommodates",
                          "type": "number",
                          "min": 0,
@@ -71,11 +77,11 @@ def create_app():
                                             'super_strict_30',
                                             'super_strict_60']},
         "cleaning_fee": {"label": "Cleaning Fee?",
-                         "type": "choice",
-                         "options": ['true', 'false']},
+                         "type": "bool",
+                         "options": ['True', 'False']},
         "instant_bookable": {"label": "Instant Bookable?",
-                             "type": "choice",
-                             "options": ['t', 'f']},
+                             "type": "bool",
+                             "options": ['True', 'False']},
         "number_of_reviews": {"label": "Number of Reviews",
                               "type": "number",
                               "min": 0,
@@ -93,7 +99,7 @@ def create_app():
     def root():
         """WebApp home page"""
         # Show the actual lanfding page with fields to enter
-        return render_template('index.html', title="Home")
+        return render_template('predict-one.html', forms=features, title="Home")
 
     @app.route('/add_listing', methods=["GET", "POST"])
     def add_listing():
@@ -101,17 +107,53 @@ def create_app():
         if request.method == "POST":
             data = request.form.to_dict(flat=False)
             for key, value in data.items():
-                listing[key] = value[0]
+                listing[key] = value
             # TODO - save listing into database
         return render_template('listing.html', title="Add a Listing", forms=features, message=f"{listing}")
 
     @app.route('/predict', methods=["POST"])
     def predict():
         """Uses trained model to make prediction on a given listing"""
-        # TODO - Import model and run prediction on listing features
-        return "Coming Soon!"
+        return render_template('predict.html', title='Home', message="Coming Soon")
 
-    @app.route('/reset')
+    @app.route('/predict-one', methods=["GET", "POST"])
+    def predict_one():
+        """Uses trained model to make prediction on a given listing"""
+        feature_order = ["property_type",
+                         "room_type",
+                         "accommodates",
+                         "bathrooms",
+                         "bed_type",
+                         "cancellation_policy",
+                         "cleaning_fee",
+                         "city",
+                         "instant_bookable",
+                         "number_of_reviews",
+                         "review_scores_rating",
+                         "bedrooms",
+                         "beds"]
+        airbnb_model = load("model.joblib")
+        predict_message = f"{listing}"
+        # Retrieve and transform data from forms
+        if request.method == "POST":
+            data = request.form.to_dict(flat=False)
+            for key, value in data.items():
+                if features[key]['type'] == "number":
+                    listing[key] = [float(value[0])]
+                elif features[key]['type'] == "bool":
+                    listing[key] = [bool(value[0])]
+                    print(listing[key])
+                else:
+                    listing[key] = value
+            data_types = {"cleaning_fee": bool, "instant_bookable": bool}
+            df = pd.DataFrame(data)[feature_order].astype(data_types)
+
+            get_price = airbnb_model.predict(df)[0]
+            predict_message = f"We suggest you set your price at ${get_price:.2f}"
+
+        return render_template('predict-one.html', title="Price Finder", forms=features, message=predict_message)
+
+    @ app.route('/reset')
     def reset():
         """Resets the Listings and Hosts tables only"""
         # TODO - Drop Listings and Hosts table, create all tables again
@@ -119,18 +161,30 @@ def create_app():
 
     # SECRET ROUTES
 
-    @app.route('/examples')
+    @ app.route('/examples')
     def examples():
         """Adds examples to the database"""
-        #Model order: propertytype roomtype accom bathrooms bedtype cancell cleaning city inst numrev revscore bedrooms beds
-        features_populated = {"property_type": ['Apartment'], "room_type": ["Entire home/apt"], "accommodates": [16], "bathrooms": [8], "bed_type": ['Real Bed'], "cancellation_policy": ['strict'], "cleaning_fee": [True], "city": ['NYC'], "instant_bookable": [True], "number_of_reviews": [605], "review_scores_rating": [100], "bedrooms": [10], "beds": [18]}
-        
+        # Model order: propertytype roomtype accom bathrooms bedtype cancell cleaning city inst numrev revscore bedrooms beds
+        features_populated = {"property_type": ['Apartment'],
+                              "room_type": ["Entire home/apt"],
+                              "accommodates": [16],
+                              "bathrooms": [8],
+                              "bed_type": ['Real Bed'],
+                              "cancellation_policy": ['strict'],
+                              "cleaning_fee": [True],
+                              "city": ['NYC'],
+                              "instant_bookable": [True],
+                              "number_of_reviews": [605],
+                              "review_scores_rating": [100],
+                              "bedrooms": [10],
+                              "beds": [18]}
+
         test_array = pd.DataFrame(data=features_populated)
         airbnb_model = load("model.joblib")
         get_price = str(airbnb_model.predict(test_array))
         return get_price
 
-    @app.route('/test-db')
+    @ app.route('/test-db')
     def test_db():
         DB.drop_all()
         DB.create_all()
